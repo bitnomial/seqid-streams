70fde85272f957689082e5715cc6759f108de23d
Brad Sherman <brad.sherman@bitnomial.com>
2021-08-10 15:35:12 -0500

    Run fourmolu

--- src/lib/System/IO/Streams/SequenceId.hs
+++ src/lib/System/IO/Streams/SequenceId.hs
@@ -1,20 +1,24 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE TupleSections #-}
 
-module System.IO.Streams.SequenceId
-       ( sequenceIdInputStream
-       , sequenceIdOutputStream
-       ) where
+module System.IO.Streams.SequenceId (
+    sequenceIdInputStream,
+    sequenceIdOutputStream,
+) where
 
-import           Control.Applicative ((<$>))
-import           Data.IORef          (newIORef, readIORef, writeIORef, atomicModifyIORef')
-import           Data.SequenceId     (SequenceIdError, checkSeqId,
-                                      incrementSeqId)
-import           System.IO.Streams   (InputStream, OutputStream)
-import qualified System.IO.Streams   as Streams
+import Control.Applicative ((<$>))
+import Data.IORef (atomicModifyIORef', newIORef, readIORef, writeIORef)
+import Data.SequenceId (
+    SequenceIdError,
+    checkSeqId,
+    incrementSeqId,
+ )
+import System.IO.Streams (InputStream, OutputStream)
+import qualified System.IO.Streams as Streams
 
 
 ------------------------------------------------------------------------------
+
 -- | Wrap an 'System.IO.Streams.InputStream' and check for dropped or duplicated sequence IDs.
 --
 -- Example:
@@ -33,13 +37,19 @@ import qualified System.IO.Streams   as Streams
 -- ghci> 'System.IO.Streams.read' is'
 -- *** Exception: user error ('Data.SequenceId.SequenceIdError' {errType = 'Data.SequenceId.SequenceIdDropped', lastSeqId = 0, currSeqId = 4})
 -- @
-sequenceIdInputStream :: Integral s
-                      => s                             -- ^ Initial sequence ID
-                      -> (a -> s)                      -- ^ Function applied to each element of the stream to get the sequence ID
-                      -> (SequenceIdError s -> IO ())  -- ^ Error handler
-                      -> InputStream a                 -- ^ 'System.IO.Streams.InputStream' to check the sequence of
-                      -> IO (InputStream a, s -> IO s) -- ^ Pass-through of the given stream, and 'IO' action that returns
-                                                       -- the current sequence id and then resets it to the initial seed
+sequenceIdInputStream ::
+    Integral s =>
+    -- | Initial sequence ID
+    s ->
+    -- | Function applied to each element of the stream to get the sequence ID
+    (a -> s) ->
+    -- | Error handler
+    (SequenceIdError s -> IO ()) ->
+    -- | 'System.IO.Streams.InputStream' to check the sequence of
+    InputStream a ->
+    -- | Pass-through of the given stream, and 'IO' action that returns
+    -- the current sequence id and then resets it to the initial seed
+    IO (InputStream a, s -> IO s)
 sequenceIdInputStream initSeqId getSeqId seqIdFaultHandler =
     inputFoldM f initSeqId
   where
@@ -50,21 +60,24 @@ sequenceIdInputStream initSeqId getSeqId seqIdFaultHandler =
 
 
 -- very slightly modified version of Streams.inputFoldM
-inputFoldM :: (a -> b -> IO a)               -- ^ fold function
-           -> a                              -- ^ initial seed
-           -> InputStream b                  -- ^ input stream
-           -> IO (InputStream b, a -> IO a)  -- ^ returns a new stream as well as an IO action to fetch and reset
-                                             -- the updated seed value.
+inputFoldM ::
+    -- | fold function
+    (a -> b -> IO a) ->
+    -- | initial seed
+    a ->
+    -- | input stream
+    InputStream b ->
+    -- | returns a new stream as well as an IO action to fetch and reset
+    -- the updated seed value.
+    IO (InputStream b, a -> IO a)
 inputFoldM f initial stream = do
     ref <- newIORef initial
-    is  <- Streams.makeInputStream (rd ref)
+    is <- Streams.makeInputStream (rd ref)
     return (is, fetchAndReset ref)
-
   where
     twiddle _ Nothing = return Nothing
-
     twiddle ref mb@(Just x) = do
-        !z  <- readIORef ref
+        !z <- readIORef ref
         !z' <- f z x
         writeIORef ref z'
         return mb
@@ -75,6 +88,7 @@ inputFoldM f initial stream = do
 
 
 ------------------------------------------------------------------------------
+
 -- | Wrap an 'System.IO.Streams.OutputStream' to give a sequence ID for each element written.
 --
 -- Example:
@@ -93,30 +107,42 @@ inputFoldM f initial stream = do
 -- ghci> getList
 -- [(1,6),(2,7)]
 -- @
-sequenceIdOutputStream :: Integral s
-                       => s                              -- ^ Initial sequence ID
-                       -> (s -> a -> b)                  -- ^ Transformation function
-                       -> OutputStream b                 -- ^ 'System.IO.Streams.OutputStream' to count the elements of
-                       -> IO (OutputStream a, s -> IO s) -- ^ returns a new stream as well as an 'IO' action that
-                                                         -- returns the current sequence id and then resets it to
-                                                         -- the initial seed
+sequenceIdOutputStream ::
+    Integral s =>
+    -- | Initial sequence ID
+    s ->
+    -- | Transformation function
+    (s -> a -> b) ->
+    -- | 'System.IO.Streams.OutputStream' to count the elements of
+    OutputStream b ->
+    -- | returns a new stream as well as an 'IO' action that
+    -- returns the current sequence id and then resets it to
+    -- the initial seed
+    IO (OutputStream a, s -> IO s)
 sequenceIdOutputStream i f = outputFoldM f' i
-  where f' seqId bdy = (nextSeqId, f nextSeqId bdy)
-          where nextSeqId = incrementSeqId seqId
+  where
+    f' seqId bdy = (nextSeqId, f nextSeqId bdy)
+      where
+        nextSeqId = incrementSeqId seqId
 
 
 -- very slightly modified version of Streams.outputFoldM
-outputFoldM :: Integral a
-            => (a -> b -> (a, c))             -- ^ fold function
-            -> a                              -- ^ initial seed
-            -> OutputStream c                 -- ^ output stream
-            -> IO (OutputStream b, a -> IO a) -- ^ returns a new stream as well as an IO action to fetch and
-                                              -- reset the updated seed value.
+outputFoldM ::
+    Integral a =>
+    -- | fold function
+    (a -> b -> (a, c)) ->
+    -- | initial seed
+    a ->
+    -- | output stream
+    OutputStream c ->
+    -- | returns a new stream as well as an IO action to fetch and
+    -- reset the updated seed value.
+    IO (OutputStream b, a -> IO a)
 outputFoldM step initSeqId outStream = do
     ref <- newIORef initSeqId
     (,fetchAndReset ref) <$> Streams.makeOutputStream (wr ref)
   where
-    wr _ Nothing    = Streams.write Nothing outStream
+    wr _ Nothing = Streams.write Nothing outStream
     wr ref (Just x) = do
         !accum <- readIORef ref
         let (!accum', !x') = step accum x
--- src/test/test.hs
+++ src/test/test.hs
@@ -9,71 +9,74 @@ import Data.SequenceId (SequenceIdError (..), SequenceIdErrorType (..))
 import qualified System.IO.Streams as Streams
 import System.IO.Streams.SequenceId (sequenceIdInputStream, sequenceIdOutputStream)
 
+
 main = defaultMain tests
+
 
 newtype SeqIdException = SeqIdException (SequenceIdError Int) deriving (Show, Eq)
 instance Exception SeqIdException
 
+
 tests :: TestTree
-tests = testGroup "seqid-streams tests"
-  [ testCase "sequenceIdInputStream can be reset" $ do
+tests =
+    testGroup
+        "seqid-streams tests"
+        [ testCase "sequenceIdInputStream can be reset" $ do
+            is <- Streams.fromList [1, 2, 3, 1, 5, 2 :: Int]
+            (is', resetSeqId) <- sequenceIdInputStream 0 id (throw . SeqIdException) is
+            r1 <- Streams.read is'
+            r1 @?= Just 1
 
-    is <- Streams.fromList [1,2,3,1,5,2 :: Int]
-    (is', resetSeqId) <- sequenceIdInputStream 0 id (throw . SeqIdException) is
-    r1 <- Streams.read is'
-    r1 @?= Just 1
+            r2 <- Streams.read is'
+            r2 @?= Just 2
 
-    r2 <- Streams.read is'
-    r2 @?= Just 2
+            r3 <- Streams.read is'
+            r3 @?= Just 3
 
-    r3 <- Streams.read is'
-    r3 @?= Just 3
+            s1 <- resetSeqId 0
+            s1 @?= 3
 
-    s1 <- resetSeqId 0
-    s1 @?= 3
+            r4 <- Streams.read is'
+            r4 @?= Just 1
 
-    r4 <- Streams.read is'
-    r4 @?= Just 1
+            s2 <- resetSeqId 4
+            s2 @?= 1
 
-    s2 <- resetSeqId 4
-    s2 @?= 1
+            r5 <- Streams.read is'
+            r5 @?= Just 5
 
-    r5 <- Streams.read is'
-    r5 @?= Just 5
+            res <- try (Streams.read is')
+            case res of
+                Left e -> e @?= SeqIdException (SequenceIdError SequenceIdDuplicated 5 2)
+                Right _ -> assertFailure "Streams.read should have failed!"
 
-    res <- try (Streams.read is')
-    case res of
-      Left e -> e @?= SeqIdException (SequenceIdError SequenceIdDuplicated 5 2)
-      Right _ -> assertFailure "Streams.read should have failed!"
+            pure ()
+        , testCase "sequenceIdOutputStream can be reset" $ do
+            (os, getList) <- Streams.listOutputStream :: IO (Streams.OutputStream (Int, Int), IO [(Int, Int)])
+            (outStream', resetSeqId) <- sequenceIdOutputStream 0 (,) os
+            Streams.write (Just 6) outStream'
+            Streams.write (Just 7) outStream'
 
-    pure ()
+            l1 <- getList
+            l1 @?= [(1, 6), (2, 7)]
 
-  , testCase "sequenceIdOutputStream can be reset" $ do
-      (os, getList) <- Streams.listOutputStream :: IO (Streams.OutputStream (Int,Int), IO [(Int,Int)])
-      (outStream', resetSeqId) <- sequenceIdOutputStream 0 (,) os
-      Streams.write (Just 6) outStream'
-      Streams.write (Just 7) outStream'
+            s1 <- resetSeqId 0
+            s1 @?= 2
 
-      l1 <- getList
-      l1 @?= [(1,6),(2,7)]
+            Streams.write (Just 8) outStream'
+            Streams.write (Just 9) outStream'
 
-      s1 <- resetSeqId 0
-      s1 @?= 2
+            l2 <- getList
+            l2 @?= [(1, 8), (2, 9)]
 
-      Streams.write (Just 8) outStream'
-      Streams.write (Just 9) outStream'
+            s1 <- resetSeqId 10
+            s1 @?= 2
 
-      l2 <- getList
-      l2 @?= [(1,8),(2,9)]
+            Streams.write (Just 10) outStream'
+            Streams.write (Just 11) outStream'
 
-      s1 <- resetSeqId 10
-      s1 @?= 2
+            l2 <- getList
+            l2 @?= [(11, 10), (12, 11)]
 
-      Streams.write (Just 10) outStream'
-      Streams.write (Just 11) outStream'
-
-      l2 <- getList
-      l2 @?= [(11,10),(12,11)]
-
-      pure ()
-  ]
+            pure ()
+        ]
